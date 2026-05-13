b2d44bdabf88c08d7c6c17e5e9ff8dc4fa996633
mtothejensen <moje1000@stud.ek.dk>
2026-05-13 11:41:34 +0200

    Chairman unit test added (member sort by name + age)

--- test/ui/ChairmanTest.java
+++ test/ui/ChairmanTest.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ChairmanRequirementsTest {
+class ChairmanTest {
 
     @Test
     void shouldSortMembersByNameForChairmanOverview() {
